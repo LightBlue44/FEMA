ccade28f3b54f0fd4dd91dcdd37c06a7703879fe
LightBlue44 <thalesgirotto@gmail.com>
2022-09-22 21:34:06 -0300

    tarefa lista v2

---
+++ PROGRAMACAO_WEB_II/22_09_22/project-name/src/tarefa.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
+
+@Controller()
+export class TarefaController {
+  tarefaLista = [];
+
+  @Get("/tarefa")
+  listaTarefa() {
+    return this.tarefaLista;
+  }
+
+  @Put("/tarefa")
+  salvarTarefa(@Body() tarefa) {
+    let index = this.tarefaLista.findIndex(t => t.codigo == tarefa.codigo);
+    if (index >= 0){
+      this.tarefaLista[index].descricao = tarefa.descricao;
+    } else {
+      this.tarefaLista.push(tarefa);
+    }
+    return "ok";
+  }
+
+  @Get("/tarefa/:codigo")
+  buscarPorCodigo(@Param() parametro) {
+    console.log(parametro.codigo);
+    let tarefa = this.tarefaLista.find(tarefa => tarefa.codigo ==parametro.codigo);
+    return tarefa;
+  }
+
+  @Delete("/tarefa/:codigo")
+  excluirTarefa(@Param() parametro){
+    let index = this.tarefaLista.findIndex(tarefa => tarefa.codigo == parametro.codigo);
+    this.tarefaLista.splice(index, 1);
+    return "ok";
+  }
+}
